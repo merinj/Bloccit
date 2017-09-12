135c2df0a9d2123be02c78f3f7b17180fab68a87
Merin Joseph <merinsony18@gmail.com>
2017-09-11 23:26:32 -0700

    assignment-25-signIn:completed

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -22,4 +22,9 @@ class UsersController < ApplicationController
        render :new
      end
    end   
+
+  def avatar_url(user)
+     gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+     "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+   end
 end
