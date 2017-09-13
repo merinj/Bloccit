006bb854f091f4696bb7e2a6f1cc12a5247ee7d4
Merin Joseph <merinsony18@gmail.com>
2017-09-12 22:03:47 -0700

    checkpoint-26-PostandUsers:completed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,15 @@
  require 'random_data'
 
+ # Create Users
+ 5.times do
+   User.create!(
+   name:     RandomData.random_name,
+   email:    RandomData.random_email,
+   password: RandomData.random_sentence
+   )
+ end
+ users = User.all
+
  # Create Topics
  15.times do
    Topic.create!(
@@ -12,9 +22,8 @@
 
  # Create Posts
  50.times do
- # #1
    Post.create!(
- # #2
+     user:   users.sample,
      topic:  topics.sample,
      title:  RandomData.random_sentence,
      body:   RandomData.random_paragraph
@@ -23,16 +32,23 @@
  posts = Post.all
  
  # Create Comments
- # #3
+ 
  100.times do
    Comment.create!(
- # #4
      post: posts.sample,
      body: RandomData.random_paragraph
    )
  end
+
+
+ user = User.first
+ user.update_attributes!(
+   email: 'meryde8@gmail.com', 
+   password: 'helloworld'
+ )
  
  puts "Seed finished"
+ puts "#{User.count} users created"
  puts "#{Topic.count} topics created"
  puts "#{Post.count} posts created"
  puts "#{Comment.count} comments created"
