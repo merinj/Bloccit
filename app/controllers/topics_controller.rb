f9be22582b262d12224d00b26d1bd729c3c1c2cc
Merin Joseph <merinsony18@gmail.com>
2017-09-13 23:48:35 -0700

    assignment-27-authorize:partly complete

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,7 +2,8 @@ class TopicsController < ApplicationController
 
   before_action :require_sign_in, except: [:index, :show]
 
-  before_action :authorize_user, except: [:index, :show]
+  before_action :authorize_user, except: [:index, :show, :update]
+
 
 	def index
      @topics = Topic.all
@@ -67,9 +68,10 @@ class TopicsController < ApplicationController
  
    def authorize_user
     
-     unless current_user.admin?
+     unless current_user.admin? || current_user.moderator?
        flash[:alert] = "You must be an admin to do that."
        redirect_to topics_path
      end
    end
+
 end
