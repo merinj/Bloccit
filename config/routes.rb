23ee768161e3ceeac231dcad5da0ed72a5f29f8f
Merin Joseph <merinsony18@gmail.com>
2017-09-17 15:59:14 -0700

    checkpoint-29-vote:completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
 
    resources :topics do
- # #34
      resources :posts, except: [:index]
    end
 
  
- # #4
+
    resources :posts, only: [] do
- # #5
      resources :comments, only: [:create, :destroy]
+
+     post '/up-vote' => 'votes#up_vote', as: :up_vote
+   	 post '/down-vote' => 'votes#down_vote', as: :down_vote
    end
 
    resources :users, only: [:new, :create]
