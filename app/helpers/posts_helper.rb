6ae278015c88af9d71c39b33412625f9019c56ef
Merin Joseph <merinsony18@gmail.com>
2017-09-21 22:30:19 -0700

    assignment-31-profiles:not completed

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -2,4 +2,9 @@ module PostsHelper
   def user_is_authorized_for_post?(post)
     current_user && (current_user == post.user || current_user.admin?)
   end
+
+  def no_post_present?(current_user)
+  	 user.post.empty?
+  	 
+  end	 	
 end
